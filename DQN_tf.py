03a6ed4ba9dbc39249a2c31de613d05756d35405
Sridhar Thiagarajan <thiagarajan14111@mech.ssn.edu.in>
2018-06-10 00:50:03 +0530

    Update DQN_tf.py
    More optimised, xavier initializer added for variables to bring stability

--- DQN_tf.py
+++ DQN_tf.py
@@ -1,13 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 22:41:30 2018
+
+@author: sritee
+"""
+
 import numpy as np
-import gym
+import gym.spaces
 import random
 import tensorflow as tf
-#env=  gym.make('MountainCar-v0') 
+
+#env=  gym.make('MountainCar-v0')
 #env = gym.make('Acrobot-v1') # change if desired to required environment
 env=gym.make('CartPole-v1') #change num_actions to 2
 num_actions=env.action_space.n #number of available actions
+env._max_episode_steps=500
 num_epochs=1000
-observations_dim=np.shape(env.observation_space.high)[0] #the observations in the environment
+observations_dim=env.observation_space.shape[0] #the observations in the environment
 gamma=0.99 #discount factor
 visualize_after_steps=10 #start the display
 memorylen=100000 #memory size of experience replay
@@ -17,66 +27,61 @@ batch=np.empty([1,observations_dim])
 emax=1
 emin=0.05
 epsilon=1  #epsilon will exponentially decrease from 1 to emin.
-dparam=0.001 #decay parameter
-tnuf=150#target network update frequency
-num_hidden_1=64
-num_hidden_2=64
+dparam=0.0005 #decay parameter
+tnuf=200#target network update frequency
+num_hidden_1=32
+num_hidden_2=32
 
-np.random.seed(1234)
+#np.random.seed(1234)
+#env.seed=1234
+#tf.set_random_seed=1234
 
-def translate(value, leftMin, leftMax, rightMin, rightMax): #this scales the state variables appropriately, called by normalize function
+if num_actions==2: #cartpole environment only
+    val1=np.array([2.5, 3.6, 0.28, 3.7]) #in case you want to set manually the limits, like in cartpole
+    val=-val1.copy()
+    print('hi')
+else:
+    val=env.observation_space.low
+    val1=env.observation_space.high
+    print('hello')
+
+def normalize(state): #this scales the state variables appropriately into 0-1 range 
    
-    leftrange = leftMax - leftMin
-    rightrange = rightMax - rightMin
-    #Convert the left range into a 0-1 range (float)
-    valueScaled = float(value - leftMin) / leftrange
-     #Convert the 0-1 range into a value in the right range.
-    return rightMin + (valueScaled * rightrange)   
-    
-def normalize(state):
-    
-    normstate=np.empty(np.shape(state))
-    if num_actions==2:
-        val=np.array([2.5, 3.6, 0.28, 3.7]) #in case you want to set manually the limits, like in cartpole
-        val1=-val
-    else:
-        val=env.observation_space.low
-        val1=env.observation_space.high
-    
-    for i in range(np.shape(state)[0]):
-        normstate[i]=translate(state[i],val[i],val1[i],0,1) #making into zero one range
-    return normstate
+       
+    valueScaled=(state-val)/(val1-val)
+    return np.zeros(observations_dim)+valueScaled *(1-0)
     
 
 x=tf.placeholder('float32',[None,observations_dim]) #input state
 y=tf.placeholder('float32',[None,num_actions]) #ground truth q values
 
 
-w1=tf.Variable(tf.random_normal([observations_dim,num_hidden_1]))
-bias1=tf.Variable(tf.random_normal([num_hidden_1]))
-mul=tf.add(tf.matmul(x,w1),bias1)
-act1=tf.nn.relu(mul)
-w2=tf.Variable(tf.random_normal([num_hidden_1,num_hidden_2]))
-bias2=tf.Variable(tf.random_normal([num_hidden_2]))
+w1=tf.get_variable("W1",shape=[observations_dim,num_hidden_1],initializer=tf.contrib.layers.xavier_initializer())
+bias1=tf.get_variable("B1",shape=[num_hidden_1],initializer=tf.contrib.layers.xavier_initializer())
+act1=tf.nn.relu(tf.add(tf.matmul(x,w1),bias1))
+w2=tf.get_variable("W2",shape=[num_hidden_1,num_hidden_2],initializer=tf.contrib.layers.xavier_initializer())
+bias2=tf.get_variable("B2",shape=[num_hidden_2],initializer=tf.contrib.layers.xavier_initializer())
 act2=tf.nn.relu(tf.add(tf.matmul(act1,w2),bias2))
-w3=tf.Variable(tf.random_normal([num_hidden_2,num_actions]))
-bias3=tf.Variable(tf.random_normal([num_actions]))
+w3=tf.get_variable("W3",shape=[num_hidden_2,num_actions],initializer=tf.contrib.layers.xavier_initializer())
+bias3=tf.get_variable("B3",shape=[num_actions],initializer=tf.contrib.layers.xavier_initializer())
 qval=tf.add(tf.matmul(act2,w3),bias3) #the outpuot q value.
 
 #target network, which has similar architecture, but different weights.
-w1_tar=tf.Variable(tf.random_normal([observations_dim,num_hidden_1]))
-bias1_tar=tf.Variable(tf.random_normal([num_hidden_1]))
+w1_tar=tf.get_variable("W1_tar",shape=[observations_dim,num_hidden_1],initializer=tf.contrib.layers.xavier_initializer(),trainable='False')
+bias1_tar=tf.get_variable("B1_tar",shape=[num_hidden_1],initializer=tf.contrib.layers.xavier_initializer(),trainable='False')
 act1_tar=tf.nn.relu(tf.add(tf.matmul(x,w1_tar),bias1_tar))
-w2_tar=tf.Variable(tf.random_normal([num_hidden_1,num_hidden_2]))
-bias2_tar=tf.Variable(tf.random_normal([num_hidden_2]))
-act2_tar=tf.nn.relu(tf.add(tf.matmul(act1,w2_tar),bias2))
-w3_tar=tf.Variable(tf.random_normal([num_hidden_2,num_actions]))
-bias3_tar=tf.Variable(tf.random_normal([num_actions]))
+w2_tar=tf.get_variable("W2_tar",shape=[num_hidden_1,num_hidden_2],initializer=tf.contrib.layers.xavier_initializer(),trainable='False')
+bias2_tar=tf.get_variable("B2_tar",shape=[num_hidden_2],initializer=tf.contrib.layers.xavier_initializer(),trainable='False')
+act2_tar=tf.nn.relu(tf.add(tf.matmul(act1_tar,w2_tar),bias2_tar))
+w3_tar=tf.get_variable("W3_tar",shape=[num_hidden_2,num_actions],initializer=tf.contrib.layers.xavier_initializer(),trainable='False')
+bias3_tar=tf.get_variable("B3_tar",shape=[num_actions],initializer=tf.contrib.layers.xavier_initializer(),trainable='False')
 qval_tar=tf.add(tf.matmul(act2_tar,w3_tar),bias3_tar)
-
 loss=tf.reduce_mean(tf.square(qval-y)) #mean squared loss
-opt=tf.train.AdamOptimizer().minimize(loss) #optimizer
-
+#grads=tf.gradients(loss,tf.trainable_variables())
+#grads_and_vars=list(zip(grads,tf.trainable_variables()))
+opt=tf.train.AdamOptimizer(3e-4).minimize(loss) #optimizer
+#minimize=opt.apply_gradients(grads_and_vars)
+#opt=tf.train.AdamOptimizer(1e-4).minimize(loss) #optimizer
 sess=tf.InteractiveSession() #run session
 init_op=tf.initialize_all_variables()
 sess.run(init_op)
@@ -97,9 +102,9 @@ def pick_action(curstate): #pick the action epsilon greedily.
         action = (np.argmax(qvalues))
     return action
     
-def compute_target(curstate): #qvalue computation by target network.
-    qvalues = sess.run(qval_tar,{x:curstate.reshape(-1,observations_dim)})
-    return qvalues
+#def compute_target(curstate): #qvalue computation by target network.
+#    qvalues = sess.run(qval_tar,{x:curstate.reshape(-1,observations_dim)})
+#    return qvalues
     
     
 def updateweights(replay): #experience replay steps.
@@ -113,7 +118,7 @@ def updateweights(replay): #experience replay steps.
     snext = np.array([ (no_state if d[2] is None else d[2]) for d in minibatch ])
 
     predict_cur = sess.run(qval,{x:states.reshape(-1,observations_dim)})
-    predict_next = sess.run(qval,{x:snext.reshape(-1,observations_dim)})
+    predict_next = sess.run(qval_tar,{x:snext.reshape(-1,observations_dim)})
 
     x_ = np.zeros((batch_len, observations_dim))
     y_ = np.zeros((batch_len, num_actions))
@@ -135,17 +140,22 @@ def updateweights(replay): #experience replay steps.
         y_[k] = m
 
     sess.run(opt,feed_dict={x:x_,y:y_})
-    
+    return [x_,y_]
 
 #model.summary()
 
 replay=[]
+cnt=0
 for idx,i in enumerate(range(int(num_epochs))):
     
     curstate = env.reset()
     csn=normalize(curstate) #Normalized state values fed to the network
+   
     while(1):
+        if idx%10==0:
+            env.render()
         steps[idx]+=1
+        cnt+=1
         #env.render()
         curaction=pick_action(csn)
         nextstate,reward, done, info = env.step(curaction) #environment simulator called
@@ -153,12 +163,12 @@ for idx,i in enumerate(range(int(num_epochs))):
         nsn=normalize(nextstate)
         
         
-        replay.append((csn,curaction,nsn,reward,done))
+        replay.append((csn,curaction,nsn,np.clip(reward,-1,1),done))
         
-        if (len(replay) > memorylen): 
+        if (len(replay) >= memorylen): 
             replay.pop(0) #memory has been filled, start refill
             
-        model=updateweights(replay) #updating the weights by calling experience replay function.
+        updateweights(replay) #updating the weights by calling experience replay function.
         
         if done or steps[idx]>2000:
             print('Episode %d ended in %d steps' %(idx,steps[idx]))
@@ -168,11 +178,10 @@ for idx,i in enumerate(range(int(num_epochs))):
         curstate=nextstate
         csn=nsn
         
-        if steps[idx]%tnuf==0: #update every 25 steps
-            
+        if cnt%tnuf==0: #update every 25 steps
             epsilon = emin + (emax - emin)*np.exp(-dparam* np.sum(steps)) #decaying the exploration parameter epsilon.
             updatetarget()
+    print('epsilon is'+str(epsilon))
+    
      
 env.monitor.close()
-
-
